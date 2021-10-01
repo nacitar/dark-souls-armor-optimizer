256407580a9b5b78917aad33530e272a89e2f401
Jacob McIntosh <nacitar.sevaht@gmail.com>
2021-10-01 13:55:50 -0600

    Better type hints

--- equipment_optimizer/optimizer.py
+++ equipment_optimizer/optimizer.py
@@ -1,8 +1,15 @@
+from __future__ import annotations  # until Python 3.10+
 import logging
 from functools import total_ordering
 from dataclasses import dataclass
+from typing import Protocol, Any
 
 LOG = logging.getLogger(__name__)
+
+
+class WeightLike(Protocol):
+    cost: float
+    modifier: float
 
 
 @total_ordering
@@ -11,9 +18,10 @@ class Weight(object):
     cost: float
     modifier: float
 
-    def __lt__(self, other) -> bool:
-        if not isinstance(other, type(self)):
-            return NotImplemented
+    def __eq__(self, other: Any) -> bool:
+        return self.cost == other.cost and self.modifier == other.modifier
+
+    def __lt__(self, other: WeightLike) -> bool:
         # sort by smallest cost then largest modifier
         return (
             self.cost < other.cost
@@ -21,7 +29,8 @@ class Weight(object):
             and self.modifier > other.modifier
         )
 
-    def __eq__(self, other) -> bool:
-        if not isinstance(other, type(self)):
-            return NotImplemented
-        return self.cost != other.cost or self.modifier != other.modifier
+    def __add__(self, other: WeightLike) -> Weight:
+        return Weight(
+            cost=self.cost + other.cost,
+            modifier=self.modifier + other.modifier,
+        )
